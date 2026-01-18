a1f23e1072905f7396a4dd0e8a96f664a6f3ad24
Arthur Hupsel <hupsel@gmail.com>
2026-01-17 21:59:01 -0300

    Add CacheRedis

--- src/main/java/br/com/hupsel/controleestoque/ControleEstoqueApplication.java
+++ src/main/java/br/com/hupsel/controleestoque/ControleEstoqueApplication.java
@@ -2,10 +2,12 @@ package br.com.hupsel.controleestoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "br.com.hupsel.controleestoque")
+@EnableCaching
 public class ControleEstoqueApplication {
 
     public static void main(String[] args) {
